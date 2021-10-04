dad7c737fe03ba3b08a5caf56b53b05f973c6505
FabioPassos10 <61357219+FabioPassos10@users.noreply.github.com>
2021-10-04 15:43:03 -0300

    atulizações

---
+++ Exextra/QNéMaior/index.js
@@ -0,0 +1,13 @@
+function verifica() {
+  var t1 = document.getElementById("txtn1");
+  var t2 = document.getElementById("txtn2");
+
+  var txtn1 = Number(t1.value);
+  var txtn2 = Number(t2.value);
+
+  var a = document.querySelector("div#res");
+  var ver = Math.max(txtn1, txtn2);
+  var Test = ver % 2 == 0 ? "Par" : "Impar";
+
+  a.innerHTML = `O maior numero é: ${ver} e ele é ${Test} `;
+}
